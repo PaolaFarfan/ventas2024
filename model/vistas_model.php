2a4c72eb6d573fbff4c38cb348aec0a3b439600e
PaolaFarfan <astrid.farfan.taipe@gmail.com>
2024-09-26 12:58:57 -0500

    int

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -2,8 +2,8 @@
 class vistaModelo{
 
     protected static function obtener_vista($vista){
-        $palabras_permitidas =[, 'usuario', 'producto'];
-        if (in_array($vista,$palabras_permitidas)) {
+        $palabras_permitidas =['login', 'usuario', 'producto'];
+        if  (in_array($vista,$palabras_permitidas)) {
             if (is_file("./views/".$vista.".php")){
                 $contenido = "./views/".$vista.".php";
             }else{
