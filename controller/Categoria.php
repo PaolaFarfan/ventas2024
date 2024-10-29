952fb920d19babfcc7b2945fc2541aeb4022d6d2
PaolaFarfan <astrid.farfan.taipe@gmail.com>
2024-10-29 11:28:36 -0500

    clase2

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -9,8 +9,19 @@ if ($tipo=="listar") {
      //respuesta
      $arr_Respuesta =array('status'=>false,'contenido'=>'');
      $arr_Categorias = $objCategoria->obtener_categoria();
+     if (!empty($arr_Categorias)) {
+          // recordemos el array para agregar las opciones de las categorias
+          for ($i=0; $i < count($arr_Categorias); $i++) { 
+               $id_categoria = $arr_Categorias[$i]-> id;
+               $categoria = $arr_Categorias[$i]->nombre;
+               $opciones = '';
+               $arr_Categorias[$i] ->options = $opciones;
+          }
+          $arr_Respuesta ['status'] = true;
+          $arr_Respuesta ['contenido'] = $arr_Categorias;
+     }
+     echo  json_encode($arr_Respuesta);
 
-     print_r($arr_Categorias);
 }
 
 ?>
--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -13,19 +13,19 @@
 
 <div>
         <label for=""> Codigo </label> <br>
-            <input type="number" placeholder="" id="codigo" name="codigo"><br>
+            <input type="number" placeholder="" id="codigo" name="codigo" class="form-control"><br>
     </div>
     <div>
         <label for=""> Nombre </label><br>
-            <input type="text" placeholder="" id="nombre" name="nombre"><br>
+            <input type="text" placeholder="" id="nombre" name="nombre" class="form-control"><br>
     </div>
     <div>
         <label for=""> Detalle </label><br>
-            <input type="text" placeholder=""id="detalle" name="detalle"><br>
+            <input name="detalle" id="detalle" class="form-control" require></input>
     </div>
     <div>
         <label for=""> Precio </label><br>
-            <input type="text" placeholder="" id="precio" name="precio"><br>
+            <input type="text" placeholder="" id="precio" name="precio" class="form-control"><br>
     </div>
     <div>
         <label for=""> Stock Inicio </label><br>
@@ -33,19 +33,21 @@
     </div>
     <div>
         <label for=""> Categoria </label><br>
-            <input type="number" placeholder="" id="categoria" name="categoria"><br>
+        <select type="number"  class="form-control" required id="idcategoria" name="idcategoria"> 
+                <option value=""> seleccione</option>
+            </select>
     </div>
     <div>
         <label for=""> Fecha Vencimiento </label><br>
-            <input type="date" placeholder="" id="fecha_v" name="fecha_v"><br>
+            <input type="date" placeholder="" id="fecha_v" name="fecha_v" class="form-control"><br>
     </di>
     <div>
         <label for=""> Imagen </label><br>
-            <input type="text" placeholder=""  id="imagen" name="imagen"><br>
+            <input type="text" placeholder=""  id="imagen" name="imagen" class="form-control"><br>
     </div>
     <div>
         <label for=""> Proveedor </label><br>
-            <input type="number" placeholder="" id="proveedor" name="proveedor"><br>
+            <input type="number" placeholder="" id="proveedor" name="proveedor" class="form-control"><br>
     </div> <br>
      <div>
         <button type="button" class="btn btn-succes"
